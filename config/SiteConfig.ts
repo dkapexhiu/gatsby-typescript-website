0f9b78dc329116a74e9727b4e8e836b896f3ec09
dkapexhiu <daniel.kapexhiu@gmail.com>
2020-06-14 00:10:58 +0200

    first changes

---
+++ config/SiteConfig.ts
@@ -0,0 +1,36 @@
+export default {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Typescript Website', // Navigation and Site Title
+  siteTitleAlt: 'Typescript Website - Gatsby Starter', // Alternative Site title for SEO
+  siteUrl: '', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteBanner: '/assets/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  defaultBg: '/assets/bg.png', // default post background header
+  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteDescription: 'Typescript Website', // Your site description
+  author: '', // Author for schemaORGJSONLD
+  siteLogo: '/assets/logo.png', // Image for schemaORGJSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '', // Twitter Username - Optional
+  ogSiteName: '', // Facebook Site Name - Optional
+  ogLanguage: 'en_US', // Facebook Language
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Settings for typography.ts
+  headerFontFamily: 'Bitter',
+  bodyFontFamily: 'Open Sans',
+  baseFontSize: '18px',
+
+  // Social media
+  siteFBAppID: '',
+
+  //
+  Google_Tag_Manager_ID: 'GTM-XXXXXXX',
+  POST_PER_PAGE: 4,
+};
